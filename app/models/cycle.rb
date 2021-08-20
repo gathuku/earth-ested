f9f604b10a0c573c8685ec976661cd3c5344b8a4
Moses Gathuku <mosesgathuku95@gmail.com>
2021-08-20 14:25:42 +0300

    calculate impact total with recursion

--- app/models/cycle.rb
+++ app/models/cycle.rb
@@ -1,14 +1,32 @@
 class Cycle < ApplicationRecord
+  # Constants
+  # Includes
+  # Asssociation
+  # Scopes
+  # Attributes
+
   attribute :components
-  # accepts_nested_attributes_for :components
 
+  # Accessors
+  # Validations
+  # Class methods / Singleton Methods
   # Instance methods
-
   def total_impact
-    total = 0
+    component_total(components)
+  end
+
+  private
+
+  # Get the total impacts value of a components with recursion
+  def component_total(components, total = 0)
     components.each do |comp|
-      total += comp['impacts'].values.map(&:to_i).sum
+      total += impact_total(comp) if comp['impacts'].present?
+      total += component_total(comp['components']) if comp['components'].present?
     end
     total
+  end
+
+  def impact_total(comp)
+    comp['impacts'].values.map(&:to_i).sum
   end
 end
